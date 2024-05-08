02507a46c86dac02e554fc4a07689bb043519956
Muath Dawood <muath.dawood.89@gmail.com>
2024-05-08 21:09:14 +0000

    refactor: split read prompt into echo and read

--- number_guess.sh
+++ number_guess.sh
@@ -3,7 +3,8 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 SECRET_NUM=$(echo $((1 + $RANDOM % 1000)))
 
-read -p "Enter your username:" USERNAME
+echo -e "Enter your username:\n"
+read USERNAME
 
 USER_ID=$($PSQL "select user_id from users where username = '$USERNAME';")
 
