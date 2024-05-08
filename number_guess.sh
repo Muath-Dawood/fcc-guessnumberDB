7090401fc3f2e69d9ab646127040755d6070c6a8
Muath Dawood <muath.dawood.89@gmail.com>
2024-05-08 21:06:51 +0000

    feat: check if user dose not exists

--- number_guess.sh
+++ number_guess.sh
@@ -6,3 +6,8 @@ SECRET_NUM=$(echo $((1 + $RANDOM % 1000)))
 read -p "Enter your username:" USERNAME
 
 USER_ID=$($PSQL "select user_id from users where username = '$USERNAME';")
+
+if [[ -z $USER_ID ]]
+then
+  echo -e "\nWelcome, $USERNAME! It look like this is your first time here."
+fi
