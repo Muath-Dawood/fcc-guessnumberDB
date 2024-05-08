bae4612ae66c7a81a25c35ee41a458f848c36d5a
Muath Dawood <muath.dawood.89@gmail.com>
2024-05-08 21:00:53 +0000

    fix: change databse name

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=numberGuess -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 SECRET_NUM=$(echo $((1 + $RANDOM % 1000)))
 
 read -p "Enter your username:" USERNAME
