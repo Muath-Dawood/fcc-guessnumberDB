cda757b30b52d81402ea6613c0675259c177ec69
Muath Dawood <muath.dawood.89@gmail.com>
2024-05-08 21:01:36 +0000

    feat: get user from database

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,5 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 SECRET_NUM=$(echo $((1 + $RANDOM % 1000)))
 
 read -p "Enter your username:" USERNAME
+
+USER_ID=$($PSQL "select user_id from users where username = '$USERNAME';")
