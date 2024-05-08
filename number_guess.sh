121d2cc643a26bdbb805fb2d77c01b2e7d160e1e
Muath Dawood <muath.dawood.89@gmail.com>
2024-05-08 21:32:10 +0000

    feat: get user info if exists

--- number_guess.sh
+++ number_guess.sh
@@ -12,4 +12,8 @@ if [[ -z $USER_ID ]]
 then
   echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
   INSERT_USER_RESULT=$($PSQL "insert into users (username) values ('$USERNAME');")
+else
+  GAMES_COUNT=$($PSQL "select count(*) from games where user_id = $USER_ID;")
+  HIGH_SCORE=$($PSQL "select max(score) from games where user_id = $USER_ID;")
+  echo -e "\nWelcome back, $USERNAME! You have played $GAMES_COUNT games, and your best game took $HIGH_SCORE guesses."
 fi
